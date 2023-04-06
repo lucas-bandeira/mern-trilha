d74e5f5d224b7d8fe7e847deeec97947f63c560a
lucas bandeira <lucas4workl8tr@gmail.com>
2023-04-06 15:48:19 -0300

    autenticacao com token jwt

--- Mern-trilha-back/config/routes.rb
+++ Mern-trilha-back/config/routes.rb
@@ -1,4 +1,12 @@
+# Rails.application.routes.draw do
+#   devise_for :users
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
+  get '/member-data', to: 'members#show'
 end
