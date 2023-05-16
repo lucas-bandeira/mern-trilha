afa3c0a8640a0cc295a09a87dac2f01f9c2c090b
Higor Rocha <higor_36rocha@hotmail.com>
2023-05-16 13:41:49 -0300

    Layout e Refatoração do Login

--- Mern-trilha-front/src/pages/Login/index.jsx
+++ Mern-trilha-front/src/pages/Login/index.jsx
@@ -1,116 +1,129 @@
-import React, {useState} from "react";
-import {Header} from "../../components/Header";
+import React, { useState } from 'react'
+import { Header } from '../../components/Header'
 import './styles.css'
 
-import {useForm} from 'react-hook-form'
-import * as yup from "yup";
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
-
-import {setNomeUsuario, login, setIdUsuario, setTipoUsuario} from '../../services/auth';
-import {yupResolver} from "@hookform/resolvers/yup";
-import api from "../../services/api";
+import {
+   setNomeUsuario,
+   login,
+   setIdUsuario,
+   setTipoUsuario
+} from '../../services/auth'
+import { yupResolver } from '@hookform/resolvers/yup'
+import api from '../../services/api'
 
 const validationPost = yup.object().shape({
-  email: yup.string().required("o email é obrigatório").max(50, "O email precisa ter menosde 50 caracteres"),
-  senha: yup.string().required("A senha é obrigatório").max(20, "A senha precisa ter menosde 20 caracteres")
+   email: yup
+      .string()
+      .required('o email é obrigatório')
+      .max(50, 'O email precisa ter menosde 50 caracteres'),
+   senha: yup
+      .string()
+      .required('A senha é obrigatório')
+      .max(20, 'A senha precisa ter menosde 20 caracteres')
 })
 
 function Login() {
-  const {formState: {errors}} = useForm({
-    resolver: yupResolver(validationPost)
-  })
+   const {
+      formState: { errors }
+   } = useForm({
+      resolver: yupResolver(validationPost)
+   })
 
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [loading, setLoading] = useState(false);
+   const [email, setEmail] = useState('')
+   const [senha, setSenha] = useState('')
+   const [loading, setLoading] = useState(false)
 
-  async function handleSubmit() {
-
-    await api.post('/api/usuarios/login', {email, senha})
-      .then(res => {
-        if (res.status === 200) {
-          if (res.data.status === 1) {
-            login(res.data.token);
-            setIdUsuario(res.data.id_client);
-            setNomeUsuario(res.data.user_name);
-            setTipoUsuario(res.data.user_type);
-            window.location.href = '/'
-          } else if (res.data.status === 2) {
-            alert('Atenção: ' + res.data.error);
-          }
-          setLoading(false);
-        } else {
-          alert('Erro no servidor');
-          setLoading(false);
-        }
+   async function handleSubmit() {
+      await api.post('/api/usuarios/login', { email, senha }).then((res) => {
+         if (res.status === 200) {
+            if (res.data.status === 1) {
+               login(res.data.token)
+               setIdUsuario(res.data.id_client)
+               setNomeUsuario(res.data.user_name)
+               setTipoUsuario(res.data.user_type)
+               window.location.href = '/'
+            } else if (res.data.status === 2) {
+               alert('Atenção: ' + res.data.error)
+            }
+            setLoading(false)
+         } else {
+            alert('Erro no servidor')
+            setLoading(false)
+         }
       })
-  }
+   }
 
-  function loadSubmit() {
-    setLoading(true);
-    setTimeout(
-      () => handleSubmit(),
-      2000
-    )
-  }
+   function loadSubmit() {
+      setLoading(true)
+      setTimeout(() => handleSubmit(), 2000)
+   }
 
-  return (
-    <>
-      <Header/>
+   return (
+      <div>
+         <Header />
 
-      <body>
-        <div>
-
-        <div className="conjunto">
-          <div className="bloco"></div>
-        </div>
-
-
-        <img src="/imagens/logo2.png" alt="logo" id="logoImg"/>
-
-        <div className="tituloLogin">
-          Login
-        </div>
-
-        <div className="formLogin">
-
-          <form>
-
-
-            <div className="inputBox">
-              <input type='text' name='email' id='email' className="inputUser" value={email}
-                     onChange={e => setEmail(e.target.value)}></input>
-              <label htmlFor="nome" className="labelInput">Email</label>
-              <p className="error-message">{errors.email?.message}</p>
+         <div className="bloco_login">
+            <div className="bloco_img">
+               <img src="/imagens/logo2.png" alt="logo" />
             </div>
 
-            <br></br>
+            <div className="bloco_form">
+               <div className="tituloLogin">Login</div>
 
-            <div className="inputBox">
-              <input type='senha' name='senha' id='senha' className="inputUser" value={senha}
-                     onChange={e => setSenha(e.target.value)}></input>
-              <label htmlFor="nome" className="labelInput">Senha</label>
-              <p className="error-message">{errors.senha?.message}</p>
+               <div className="formLogin">
+                  <form>
+                     <div className="inputBox">
+                        <label htmlFor="nome" className="labelInput">
+                           Email
+                        </label>
+                        <input
+                           type="text"
+                           name="email"
+                           id="email"
+                           className="inputUser"
+                           value={email}
+                           onChange={(e) => setEmail(e.target.value)}
+                        ></input>
+                        <p className="error-message">{errors.email?.message}</p>
+                     </div>
+
+                     <div className="inputBox">
+                        <label htmlFor="nome" className="labelInput">
+                           Senha
+                        </label>
+                        <input
+                           type="password"
+                           name="senha"
+                           id="senha"
+                           className="inputUser"
+                           value={senha}
+                           onChange={(e) => setSenha(e.target.value)}
+                        ></input>
+
+                        <p className="error-message">{errors.senha?.message}</p>
+                     </div>
+
+                     <div className="cadastro_enviar">
+                        <a id="link" href="/cadastro">
+                           Cadastre-se aqui
+                        </a>
+                        <input
+                           type="submit"
+                           name="enviarlogin"
+                           id="enviarlogin"
+                           onClick={loadSubmit}
+                           disabled={loading}
+                        ></input>
+                     </div>
+                  </form>
+               </div>
             </div>
-
-            <br></br>
-
-            <input type="submit" name="enviarlogin" id='enviarlogin' onClick={loadSubmit} disabled={loading}></input>
-
-            <br></br>
-
-            <a id="link" href="/cadastro">Não possui casastro? Casatre-se</a>
-
-
-          </form>
-        </div>
-
-        <div className="bloco2Login"></div>
-
+         </div>
       </div>
-      </body>
-    </>
-  )
+   )
 }
 
-export default Login;
+export default Login
